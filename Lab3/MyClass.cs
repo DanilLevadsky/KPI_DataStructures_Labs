8c327eef0e23e271a89f509b2f9bda795a28d366
Danil Levadsky <danillevadsky@gmail.com>
2020-03-14 23:35:08 +0200

    updated lab3

--- Lab3/MyClass.cs
+++ Lab3/MyClass.cs
@@ -4,25 +4,18 @@ namespace Lab3
 {
     public class MyClass
     {
-        private int[,] matValue;
-        private int length;
-        private int width;
-        private int amountOfElements;
+        public int[,] Matrix { get; set; }
 
-        public int[,] Matrix { get => matValue; set => matValue = value; }
-
-        public int Length => length;
-        public int Width => width;
-
-        public int AmountOfElements => amountOfElements;
+        public int Length { get; }
+        public int Width { get; }
+        public int AmountOfElements { get; }
 
         public MyClass(int length, int width)
         {
             this.Matrix = new int[length, width];
-            this.length = Matrix.GetLength(0);
-            this.width = Matrix.GetLength(1);
-            this.amountOfElements = length * width;
-
+            this.Length = Matrix.GetLength(0);
+            this.Width = Matrix.GetLength(1);
+            this.AmountOfElements = length * width;
         }
 
         public int? this[int index1, int index2]
@@ -47,26 +40,11 @@ namespace Lab3
                 }
                 catch (IndexOutOfRangeException)
                 {
+                    Console.WriteLine("Index out of range");
                     throw;
                 }
-                
             }
         }
 
-        private void Checker(int index1, int index2) 
-        {
-            if (index1 < 0 || index1 >= Matrix.GetLength(0))
-            {
-                throw new IndexOutOfRangeException("Out of range.");
-            }
-
-            if (index2 < 0 || index2 >= Matrix.GetLength(1))
-            {
-                throw new  IndexOutOfRangeException("Out of range.");
-            }
-        }
-        
-        
-        
     }
 }
--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -4,12 +4,18 @@ namespace Lab3
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             const int width = 5;
-            const int len = 7;
-            var mat1 = new MyClass(len, width);
-            mat1.Matrix = new int[len,width] {{1,2,3,4,5}, {2,3,4,5,6}, {3,4,1,6,7}, {4,5,6,7,8}, {1,2,3,4,5}, {1,2,2,2,5}, {1,7,3,8,5}};
+            const int len = 6;
+            var mat1 = new MyClass(len, width)
+            {
+                Matrix = new int[len, width]
+                {
+                    {1, 2, 3, 4, 5}, {2, 3, 4, 5, 6}, {3, 4, 1, 6, 7},
+                    {4, 5, 6, 7, 8}, {0, 2, 2, 2, 5}, {1, 7, 3, 8, 5}
+                }
+            };
             ReadMatrix(mat1);
             mat1[2, 2] = 0;
             mat1[3, 2] = 0;
@@ -20,11 +26,17 @@ namespace Lab3
             Console.WriteLine($"Length: {mat1.Length}");
             Console.WriteLine($"Width: {mat1.Width}");
             Console.WriteLine($"Number of elements: {mat1.AmountOfElements}");
-            mat1[7, 1] = 12;
+            try {
+                mat1[7, 1] = 12;   
+            }
+            catch(IndexOutOfRangeException){
+                Console.WriteLine("Out of range");
+            }
+
         }
 
-        
-        public static void ReadMatrix(MyClass matr)
+
+        private static void ReadMatrix(MyClass matr)
         {
             for (var i = 0; i < matr.Matrix.GetLength(0); i++)
             {
@@ -32,10 +44,8 @@ namespace Lab3
                 {
                     Console.Write("{0, 4}", matr[i,j]);
                 }
-
                 Console.WriteLine();
             }
-
             Console.WriteLine();
         }
     }
